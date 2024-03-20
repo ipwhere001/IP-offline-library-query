0759a801d84088f15355bb133e57080c36cd1798
ipwhere001 <163973213+ipwhere001@users.noreply.github.com>
2024-03-20 21:24:18 +0800

    Update ip_search_v6.go

--- ipv6/golang/文件版/ip_search_v6.go
+++ ipv6/golang/文件版/ip_search_v6.go
@@ -1,4 +1,4 @@
-package ipv6distreet
+package main
 
 import (
 	"errors"
@@ -12,20 +12,21 @@ import (
 )
 
 type IpInfoV6 struct {
+	data      []byte
 	prefStart map[uint32]uint32
 	prefEnd   map[uint32]uint32
 	endArr    []*big.Int
-	addrArr   []string
+	numbers   uint32
 }
 
 var objV6 *IpInfoV6
 var onceV6 sync.Once
 
-func GetDistrictObjectV6() *IpInfoV6 {
+func GetDistrictObjectV6(file string) *IpInfoV6 {
 	onceV6.Do(func() {
 		objV6 = &IpInfoV6{}
 		var err error
-		objV6, err = LoadFileV6("conf/ipv6_district.dat")
+		objV6, err = LoadFileV6(file)
 		if err != nil {
 			log.Fatal("the IP Dat loaded failed!ipv6_district.dat")
 		}
@@ -36,14 +37,15 @@ func GetDistrictObjectV6() *IpInfoV6 {
 func LoadFileV6(file string) (*IpInfoV6, error) {
 	p := IpInfoV6{}
 	data, err := ioutil.ReadFile(file)
+	p.data = data
 	if err != nil {
 		return nil, err
 	}
 
-	numbers := UnpackInt4byteV6(data[4], data[5], data[6], data[7])
+	p.numbers = UnpackInt4byteV6(data[4], data[5], data[6], data[7])
 	p.prefStart = make(map[uint32]uint32)
 	p.prefEnd = make(map[uint32]uint32)
-	for k := uint32(0); k < numbers; k++ {
+	for k := uint32(0); k < p.numbers; k++ {
 		i := k*12 + 4 + 4
 		p.prefStart[UnpackInt4byteV6(data[i+8], data[i+9], data[i+10], data[i+11])] = UnpackInt4byteV6(data[i], data[i+1], data[i+2], data[i+3])
 		p.prefEnd[UnpackInt4byteV6(data[i+8], data[i+9], data[i+10], data[i+11])] = UnpackInt4byteV6(data[i+4], data[i+5], data[i+6], data[i+7])
@@ -53,20 +55,21 @@ func LoadFileV6(file string) (*IpInfoV6, error) {
 	RecordSize := UnpackInt4byteV6(data[0], data[1], data[2], data[3])
 
 	p.endArr = make([]*big.Int, RecordSize)
-	p.addrArr = make([]string, RecordSize)
 	for i := uint32(0); i < RecordSize; i++ {
-		j := numbers*12 + 4 + 4 + (i * 55)
-		
-		offset := UnpackInt4byteV6(data[j+50], data[1+j+50], data[2+j+50], data[3+j+50])
-		length := uint32(data[50+j+4])
+		j := p.numbers*12 + 4 + 4 + (i * 55)
 		endipnum := new(big.Int)
 		endipnumInt, _ := endipnum.SetString(strings.ReplaceAll(string(data[j:j+50]), "*", ""), 10)
-
 		p.endArr[i] = endipnumInt
-		p.addrArr[i] = string(data[offset:int(offset+length)])
 	}
 	return &p, err
 
+}
+
+func (p *IpInfoV6) GetAddr(row uint32) string {
+	j := p.numbers*12 + 4 + 4 + (row * 55)
+	offset := UnpackInt4byteV6(p.data[j+50], p.data[1+j+50], p.data[2+j+50], p.data[3+j+50])
+	length := uint32(p.data[50+j+4])
+	return string(p.data[offset:int(offset+length)])
 }
 
 func (p *IpInfoV6) GetV6(ip string) (string, error) {
@@ -74,7 +77,6 @@ func (p *IpInfoV6) GetV6(ip string) (string, error) {
 
 	parseUint, _ := strconv.ParseUint(ips[0], 16, 32)
 
-	
 	prefix := uint32(parseUint)
 
 	intIP, err := ipToIntV6(ip)
@@ -92,7 +94,7 @@ func (p *IpInfoV6) GetV6(ip string) (string, error) {
 		cur = p.SearchV6(low, high, intIP)
 	}
 
-	return p.addrArr[cur], nil
+	return p.GetAddr(cur), nil
 
 }
 
@@ -118,7 +120,7 @@ func (p *IpInfoV6) SearchV6(low uint32, high uint32, k *big.Int) uint32 {
 func ipToIntV6(ipstr string) (*big.Int, error) {
 	ip := net.ParseIP(ipstr)
 	ip = ip.To16()
-	
+
 	if ip == nil {
 		return big.NewInt(0), errors.New("invalid ipv6")
 	}
