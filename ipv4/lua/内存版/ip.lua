02a8bdea5482023ca85111681db85aa0841bbda6
ipwhere001 <163973213+ipwhere001@users.noreply.github.com>
2024-03-20 16:13:31 +0800

    Create ip.lua

---
+++ ipv4/lua/内存版/ip.lua
@@ -0,0 +1,104 @@
+ipdata = { prefStart = {}, prefEnd = {},
+    endArr = {}, addrArr = {}, data = {},
+    file = {}, _version = "0.1.2" }
+
+function unpackInt4byte(a, b, c, d)
+    return (a % 256) + (b * 256) + (c * 65536) + (d * 16777216)
+end
+
+function fsize(file)
+    local current = file:seek()
+    local size = file:seek("end")
+    file:seek("set", current)
+    return size
+end
+
+function redaFileBytes(file, length, off)
+    file:seek("set", off)
+    local str = file:read(length)
+    local bytes = {}
+    for i = 1, #str do
+        table.insert(bytes, string.byte(str, i))
+    end
+    return bytes
+end
+
+function ipdata.LoadFile(path)
+    local sizeData = {}
+
+    ipdata.file = io.open(path, "rb")
+    ipdata.data = redaFileBytes(ipdata.file, 257 * 8 + 4, 0)
+    for k = 1, 256 do
+        local i = k * 8 + 4
+        ipdata.prefStart[k] = unpackInt4byte(ipdata.data[i + 1], ipdata.data[i + 2], ipdata.data[i + 3], ipdata.data[i + 4])
+        ipdata.prefEnd[k] = unpackInt4byte(ipdata.data[i + 5], ipdata.data[i + 6], ipdata.data[i + 7], ipdata.data[i + 8])
+    end
+
+    local recordSize = unpackInt4byte(ipdata.data[1], ipdata.data[2], ipdata.data[3], ipdata.data[4])
+
+    local allsizeData = redaFileBytes(ipdata.file, recordSize*9+9, 2052)
+    for i = 0, (recordSize-1) do
+        local x = i * 9
+		local a = 1 + x
+		local b = 2 + x
+		local c = 3 + x
+		local d = 4 + x
+        local offset = unpackInt4byte(allsizeData[d + 1], allsizeData[d+2], allsizeData[d+3], allsizeData[d+4])
+        local len = allsizeData[d+5] % 256
+		local endipnum2 = unpackInt4byte(allsizeData[a], allsizeData[b], allsizeData[c], allsizeData[d])
+		ipdata.endArr[i] = endipnum2
+        ipdata.file:seek("set", offset)
+        local tempData = ipdata.file:read(len)
+        ipdata.addrArr[i] = tempData
+    end
+end
+
+
+function ip2int(ip)
+    local o1, o2, o3, o4 = string.match(ip, "(%d+)%.(%d+)%.(%d+)%.(%d+)")
+    local num = 2 ^ 24 * o1 + 2 ^ 16 * o2 + 2 ^ 8 * o3 + o4
+    return math.floor(num)
+end
+
+function ipdata.Search(low, high, k)
+    local M = 0
+    while low <= high do
+        local mid = math.floor((low + high) / 2)
+        local endipNum = ipdata.endArr[mid]
+        if endipNum == nil then
+            break
+        end
+        if endipNum >= k then
+            M = mid
+            if mid == 0 then
+                break
+            end
+            high = mid - 1
+        else
+            low = mid + 1
+        end
+    end
+    return M
+end
+
+function ipdata.FindIP(ip)
+    local ips = { string.match(ip, "([^.]+).?") }
+    local prefix = tonumber(ips[1])
+
+    local low = ipdata.prefStart[prefix]
+    local high = ipdata.prefEnd[prefix]
+    local intIP = ip2int(ip)
+
+    local cur = 0
+    if low == high then
+        cur = low
+    else
+        cur = ipdata.Search(low, high, intIP)
+    end
+
+    if cur == 100000000 then
+        return "0.0.0.0"
+    else
+        return ipdata.addrArr[cur]
+    end
+end
